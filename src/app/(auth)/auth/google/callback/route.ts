8b14a8530d0be195ab377c09a012c0e3486d72ae
arifuddincoder <arifuddincoder@gmail.com>
2026-05-09 22:36:05 +0600

    fix: update callback route file

--- src/app/(auth)/auth/google/callback/route.ts
+++ src/app/(auth)/auth/google/callback/route.ts
@@ -4,13 +4,13 @@ import { COOKIE_NAMES } from "@/utils/cookie.constants";
 export async function GET(request: NextRequest) {
 	const accessToken = request.nextUrl.searchParams.get("accessToken");
 	const refreshToken = request.nextUrl.searchParams.get("refreshToken");
-
+	const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "https://cinetube.arifuddincoder.site";
 	if (!accessToken || !refreshToken) {
-		return NextResponse.redirect(new URL("/login?error=oauth_failed", "https://cinetube.arifuddincoder.site"));
+		return NextResponse.redirect(new URL("/login?error=oauth_failed", baseUrl));
 	}
 
 	// ✅ hardcode করো অথবা env থেকে নাও
-	const response = NextResponse.redirect(new URL("/dashboard", "https://cinetube.arifuddincoder.site"));
+	const response = NextResponse.redirect(new URL("/dashboard", baseUrl));
 
 	response.cookies.set(COOKIE_NAMES.ACCESS_TOKEN, accessToken, {
 		httpOnly: true,
